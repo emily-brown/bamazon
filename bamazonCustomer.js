0b51b411999f38e206c7341683c90fab1835e383
Emily Brown <emily_brown@rogers.com>
2019-10-10 18:36:40 -0400

    reconnect with mysql to properly connect

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,13 +4,13 @@ const inquirer = require("inquirer")
 // create the connection information for the sql database
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 8889,
+    port: 3306,
 
     // Your username
     user: "root",
 
     // Your password
-    password: "root",
+    password: "password",
     database: "bamazon"
 });
 

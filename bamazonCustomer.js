fef964117c1395731f8a5f3ec5a3adc382d6bb58
Emily Brown <emily_brown@rogers.com>
2019-10-10 18:43:13 -0400

    changed to console.table

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -22,14 +22,14 @@ connection.connect(function(err) {
 });
 
 let displayInventory = () => {
-    //console.log("SQL Connection Established")
+    //console.table("SQL Connection Established")
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err
         for (let i = 0; i < res.length; i++) {
-            console.log(" - - - - - - - - - - - - - - - ")
-            console.log("item number: " + res[i].item_id)
-            console.log("item: " + res[i].product_name)
-            console.log("price: $" + res[i].price)
+            console.table(" - - - - - - - - - - - - - - - ")
+            console.table("item number: " + res[i].item_id)
+            console.table("item: " + res[i].product_name)
+            console.table("price: $" + res[i].price)
         }
         purchase()
     })
@@ -74,7 +74,7 @@ let purchase = () => {
                 if (err) throw err
 
                 if (res.length === 0) {
-                    console.log("ERROR: Invalid Item ID. Please select a valid Item ID.")
+                    console.table("ERROR: Invalid Item ID. Please select a valid Item ID.")
                     displayInventory()
                 } else {
 
@@ -82,32 +82,32 @@ let purchase = () => {
                     let productInfo = res[0]
 
                     if (quantity <= productInfo.stock_quantity) {
-                        console.log(productInfo.product_name + "is in stock! Placing order now!")
-                        console.log("\n")
+                        console.table(productInfo.product_name + "is in stock! Placing order now!")
+                        console.table("\n")
 
                         // the updating query string
                         var updateQueryStr = "UPDATE products SET stock_quantity = " + (productInfo.stock_quantity - quantity) + " WHERE item_id = " + item
-                            // console.log('updateQueryStr = ' + updateQueryStr);
+                            // console.table('updateQueryStr = ' + updateQueryStr);
 
                         // Update the inventory
                         connection.query(updateQueryStr, function(err, data) {
                             if (err) throw err;
 
-                            console.log("Your order has been placed!");
-                            console.log("Your total is $" + productInfo.price * quantity)
-                            console.log("Thank you for shopping with bamazon!")
-                            console.log(" - - - - - - - - - - - - - - - ")
-                            console.log("To shop again with us please input 'node bamazonCustomer.js' into your command line again.")
-                            console.log("\n")
+                            console.table("Your order has been placed!");
+                            console.table("Your total is $" + productInfo.price * quantity)
+                            console.table("Thank you for shopping with bamazon!")
+                            console.table(" - - - - - - - - - - - - - - - ")
+                            console.table("To shop again with us please input 'node bamazonCustomer.js' into your command line again.")
+                            console.table("\n")
 
                             // End the database connection and close the app
                             connection.end();
                         })
                     } else {
-                        console.log("Sorry, there is not enough " + productInfo.product_name + " in stock.")
-                        console.log("Your order can not be placed as is.")
-                        console.log("Please modify your order or select another item.")
-                        console.log("\n")
+                        console.table("Sorry, there is not enough " + productInfo.product_name + " in stock.")
+                        console.table("Your order can not be placed as is.")
+                        console.table("Please modify your order or select another item.")
+                        console.table("\n")
 
                         // After 3 seconds display the inventory again so that the customer can make a new selcetion.
                         setTimeout(function() { displayInventory() }, 3000)
